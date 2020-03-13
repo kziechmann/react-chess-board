3d0a45f6182dd2eb9f8346f83a8eaf373737a939
Kevin Ziechmann <kziechmann@gmail.com>
2020-03-13 17:14:32 -0600

    Limited movement based on turns, added turn indicator

--- src/App.js
+++ src/App.js
@@ -17,21 +17,21 @@ const initialBoard = [
 function App() {
   const [state, updateState] = useState({
     board: initialBoard,
+    turn: "black",
     currentPosition: [null,null],
     movingPiece: false
   })
 
   const movePiece = (currentPosition, nextPosition)=>{
-    if(isValidMove(currentPosition,nextPosition)) {
+    if(isValidMove(currentPosition, nextPosition)) {
       const [currentY,currentX] = currentPosition
       const [nextY,nextX] = nextPosition
 
       const newBoard = [...state.board]
-      
+      const turn = state.turn === 'black' ? 'white' : 'black'
       newBoard[nextY][nextX] = newBoard[currentY][currentX]
       newBoard[currentY][currentX] = "--"
-      
-      updateState({board:newBoard}) 
+      return { board:newBoard, turn }
     }    
   }
 
@@ -43,28 +43,33 @@ function App() {
     if(currentY === nextY && currentX === nextX ) return false;
     if( currentPiece === "--") return false;
     if(currentPiece[0] === nextPiece[0]) return false;
+    if(currentPiece[0].toLowerCase() !== state.turn[0].toLowerCase()) return false;
     return true;
   }
 
   const clickPiece = (position) =>{
-    const newState = {...state}
+    let newState = {...state};
     if(!state.movingPiece){
       newState.movingPiece = true
       newState.currentPosition = position
     } else {
       newState.movingPiece = false
       newState.currentPosition = [null, null]
-      movePiece(state.currentPosition, position)
+      newState = {...newState, ...movePiece(state.currentPosition, position)}
     }
     updateState(newState)
   }
 
   return (
     <div className="App">
+      <span className="app-header">
+       <h1>React Chess</h1>
+        <h2 style={{color: state.turn}}>{`{ ${state.turn}'s turn }`} </h2>
+      </span>
       <div className="chess-board" >
         <table className="chess-board_squares" cellSpacing="0">
           {state.board.map(
-            (row, i) => < BoardRow row={row} rowIdx={i} clickPiece={clickPiece} currentPosition={state.currentPosition}/>
+            (row, i) => < BoardRow key={i} row={row} rowIdx={i} clickPiece={clickPiece} currentPosition={state.currentPosition}/>
           )}
         </table>
       </div>
